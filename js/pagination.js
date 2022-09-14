be6094b4056d9155030bb5e97096ad9497474078
firentus999 <firentusagustonep@gmail.com>
2022-09-14 15:37:00 +0700

    My Portofolio

---
+++ js/pagination.js
@@ -0,0 +1,20 @@
+ function load_data(page)
+ {
+  $.ajax({
+   url:"pagedata/"+page,
+   method:"GET",
+   dataType:"json",
+   success:function(data)
+   {
+    $("#content").html(data.list_nama);
+    $("#pagination_link").html(data.list_link);
+   }
+  });
+ }
+ 
+
+ $(document).on("click", ".pagination li a", function(event){
+  event.preventDefault();
+  var page = $(this).data("ci-pagination-page");
+  load_data(page);
+ });
